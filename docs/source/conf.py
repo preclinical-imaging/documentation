75a33e2c77cca92b3a721c5d4d97aa6ec1c9596c
stl-steve-moore <moore.stephen.m@wustl.edu>
2023-08-25 15:02:32 -0500

    Minor step to test readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PIXI:'
+copyright = '2023, Washington University in St. Louis'
+author = 'Kooresh Shoghi'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
